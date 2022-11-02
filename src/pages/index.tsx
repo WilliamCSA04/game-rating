fd900deb328da9981cbb274c8fd4a1b63c95e566
williamcsa04 <williamcaetano14@gmail.com>
2022-11-02 00:31:51 -0300

    :fire: Removed hello procedure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
 import { trpc } from '../utils/trpc';
 export default function IndexPage() {
-  const hello = trpc.hello.useQuery({ text: 'client' });
   const test = trpc.getGames.useQuery(undefined, { staleTime: Infinity });
-  
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
   return (
     <div>
-      <p>{hello.data.greeting}</p>
       <p className="text-3xl font-bold underline"> teste </p>
     </div>
   );
--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,17 +2,6 @@ import { z } from 'zod';
 import { igdb, twitch } from '../services';
 import { publicProcedure, router } from '../trpc';
 export const appRouter = router({
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string().nullish(),
-      }),
-    )
-    .query(({ input }) => {
-      return {
-        greeting: `hello ${input?.text ?? 'world'}`,
-      };
-    }),
   getGames: publicProcedure
     .query(async () => {
       const twitchCredentials = await twitch.login();
